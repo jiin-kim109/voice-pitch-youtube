2c9534ccf40c17e166799420ff9885e5cc166e10
jiin-kim109 <jiin.kim109@gmail.com>
2020-06-17 11:49:28 +0900

    editing ReadMe

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const youtubeStream = require('youtube-audio-stream');
 
-router.get('/', (req, res)=> res.json({username:'bryans'}));
-
 router.get('/youtube/:videoId', (req, res) => {
     try{
         youtubeStream(req.params.videoId).pipe(res);
